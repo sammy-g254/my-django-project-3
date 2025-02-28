5bc41c38d60bb1efa9f7c0bc0a02bf800f72e854
sammy-g254 <gsam65303@gmail.com>
2025-02-28 09:53:34 +0300

    first commit

---
+++ myapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from .import views
+
+urlpatterns = [
+    path('', views.student_list, name='index'),
+    path('create/', views.create_student, name='create'),
+    #path('<int:pk>/', views.student_detail, name='detail'),
+    #path('<int:pk>/update/', views.update_student, name='update'),
+    path('delete/<int:id>/', views.delete_student, name='delete'),
+]
---
+++ myapp/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import redirect, render
+from .forms import StudentForm
+from .models import Student
+
+# Create your views here.
+
+#proj=create,read,update,delete
+#list all students
+def student_list(request):
+    students=Student.objects.all()
+    return render(request,'index.html',{'students':students})
+#create a new student
+def create_student(request):
+    if request.method=='POST':
+        form=StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form=StudentForm()      
+    return render(request,'student_form.html',{'form':form})
+#delete an old student
+def delete_student(request,id):
+    student=Student.objects.get(id=id)
+    student.delete()
+    return redirect('index')
+          
+#update an existing student
