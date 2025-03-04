8a7b2deec986d282f86390f88ced2a957ac17d37
sammy-g254 <gsam65303@gmail.com>
2025-03-04 11:12:04 +0300

    `Added user authentication functionality and updated templates and views accordingly`

--- myapp/urls.py
+++ myapp/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     #path('<int:pk>/update/', views.update_student, name='update'),
     path('delete/<int:id>/', views.delete_student, name='delete'),
     path('update/<int:id>/', views.update_student, name='update'),
-    path('register/', views.register_user, name='register')
+    path('register/', views.register_user, name='register'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout')
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import redirect, render
+from django.shortcuts import redirect
 from .forms import StudentForm, RegisterForm
 from .models import Student
+from django.contrib.auth import login
+from django.contrib.auth import logout
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 
@@ -40,6 +44,22 @@ def register_user(request):
     if request.method=='POST':
         form=RegisterForm(request.POST)
         if form.is_valid():
-            form.save()
+            user=form.save()
+            login(request,user)
             return redirect('index')
+    else:
+        form=RegisterForm()
     return render(request,'register.html',{'form':form})
+def login_user(request):
+    form=AuthenticationForm()
+    if request.method=='POST':
+        form=AuthenticationForm(request,data=request.POST)
+        if form.is_valid():
+            user=form.get_user()
+            login(request,user)
+            return redirect('index')
+    else:
+        form=AuthenticationForm()
+    return render(request,'login.html',{'form':form})
+def logout_user(request):
+    return redirect(request,'login.html')
